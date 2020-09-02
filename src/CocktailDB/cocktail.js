4c362883c54668a32d21bf0404df203ba49567ff
Kelleymb <kelleybuccieri@gmail.com>
2020-09-02 16:47:22 -0400

    cocktaildb added, not working

--- src/CocktailDB/cocktail.js
+++ src/CocktailDB/cocktail.js
@@ -1,11 +1,61 @@
 import React, {Component} from 'react';
 import './Cocktail.css'
+import OpenBrewery from '../OpenBreweryDB/OpenBrewery';
 
 class Cocktail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: [],
+        };
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+        fetch(url, {
+            "method": "GET" 
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Something went wrong. Please try again.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            this.setState({
+                results: [
+                    ...data,
+                ]  
+            });
+        })
+        .catch(err => {
+            this.setState({
+              error: err.message
+            });
+        });
+
+    }
+
+
+
     render() {
+
         return (
-            <section>
-                <h2></h2>
+            <section className="cocktail-db">
+                <section className="info">
+                    <p>Not sure what you want? Generate a recipe at random courtesy of <a href="https://www.thecocktaildb.com/" target="_blank">Cocktail DB API</a></p>
+                </section>
+                <form className="cocktail-db-api" onSubmit={this.handleSubmit}>
+                    <button type="submit" id="random" name="random">Generate random drink!</button>
+                </form>    
+                <section className="api-result">
+                    <div className="result">
+                        {this.state.results}
+                    </div>
+                </section>
             </section>
         );
     }
--- src/Collection/collection.js
+++ src/Collection/collection.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Cocktail from '../CocktailDB/Cocktail';
 import OpenBrewery from '../OpenBreweryDB/OpenBrewery';
-import './Collection.css'
+import './Collection.css';
 
 class Collection extends Component {
     render() {
         return (
             <>
+                <Cocktail />
                 <OpenBrewery />
                 <section>
                     <h2></h2>
