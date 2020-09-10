cada78d4c4699509e3c5393ce5912092064dfad1
Kelleymb <kelleybuccieri@gmail.com>
2020-09-10 11:07:05 -0400

    Home test passing

--- src/Home/home.test.js
+++ src/Home/home.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
+import { BrowserRouter } from 'react-router-dom';
 
 describe(`Home Component`, () => {
     it(`renders without errors`, () => {
         const section = document.createElement('section');
-        ReactDOM.render(<Home />, section);
+        ReactDOM.render(<BrowserRouter><Home /></BrowserRouter>, section);
         ReactDOM.unmountComponentAtNode(section);
     })
 })
